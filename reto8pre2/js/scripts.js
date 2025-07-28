b589bc96948a04456f906e5b2f3a593f291bad20
Nicolas-pino214 <nico.pino214@gmail.com>
2025-07-28 11:41:47 -0500

    reto terminado

--- reto8pre2/js/scripts.js
+++ reto8pre2/js/scripts.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
-    const inputBox = document.getElementById('inputBox')
-})
+    const inputBox = document.getElementById('inputBox');
+    const outputBox =document.getElementById('outputBox');
+
+    inputBox.addEventListener('input', function(){
+        const message = inputBox.value.toUpperCase();
+    outputBox.textContent = message;
+    });
+
+
+});
